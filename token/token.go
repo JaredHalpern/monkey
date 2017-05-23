78b1ce0c221645216c4e57535d19d3be8e43cf26
Jared Halpern <jareeedo@gmail.com>
2017-05-23 07:21:01 -0400

    first commit

---
+++ token/token.go
@@ -0,0 +1,36 @@
+// token/token.go
+
+package token
+
+type TokenType string
+
+type Token struct {
+	Type	TokenType
+	Literal	string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF	= "EOF"
+
+	// Identifiers and Literals
+	IDENT	= "IDENT"
+	INT	= "INT"
+
+	// Operators
+	ASSIGN	= "="
+	PLUS	= "+"
+
+	// Delimiters
+	COMMA	= ","
+	SEMICOLON	= ";"
+
+	LPAREN		= "("
+	RPAREN		= ")"
+	LBRACE		= "{"
+	RBRACE		= "}"
+
+	// Keywords
+	FUNCTION	= "FUNCTION"
+	LET		= "LET"
+)
