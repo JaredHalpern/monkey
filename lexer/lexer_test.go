8d842e64dc525fbeba8c4712cb89834cb0021be0
Jared Halpern <jareeedo@gmail.com>
2017-05-26 08:34:14 -0400

    wip

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,7 +6,13 @@ import (
 		)
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `let five = 5;
+	let ten = 10;
+	let add = fn(x, y) {
+		x + y;
+		};
+	let result = add(five, ten);
+	`
 	
 	tests := []struct {
 		expectedType	token.TokenType
