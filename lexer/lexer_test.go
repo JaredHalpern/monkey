0b847b539d3d768d5bea6682e9ffb2671a912735
Jared Halpern <jareeedo@gmail.com>
2017-05-31 08:30:32 -0400

    add == and !=

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -8,9 +8,11 @@ import (
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
 	let ten = 10;
+
 	let add = fn(x, y) {
 		x + y;
 		};
+
 	let result = add(five, ten);
 	!-/*5;
 	5 < 10 > 5;
@@ -19,7 +21,11 @@ func TestNextToken(t *testing.T) {
 		return true;
 	} else {
 		return false;
-	}`
+	}
+
+	10 == 10;
+	10 != 9;
+	`
 	
 	tests := []struct {
 		expectedType	token.TokenType
@@ -90,6 +96,14 @@ func TestNextToken(t *testing.T) {
 	{token.FALSE, "false"},
 	{token.SEMICOLON, ";"},
 	{token.RBRACE, "}"},	
+	{token.INT, "10"},
+	{token.EQ, "=="},
+	{token.INT, "10"},
+	{token.SEMICOLON, ";"},
+	{token.INT, "10"},
+	{token.NOT_EQ, "!="},
+	{token.INT, "9"},
+	{token.SEMICOLON, ";"},
 	{token.EOF, ""},
 }
 
