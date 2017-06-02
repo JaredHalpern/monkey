c5c8a8dd8a41dcb8cd7a1632cf7cd704f035b4a5
Jared Halpern <jareeedo@gmail.com>
2017-06-02 08:44:07 -0400

    more parser work

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,30 @@
 package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+	)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// read two tokens
+	// set curToken and peekToken
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
+
