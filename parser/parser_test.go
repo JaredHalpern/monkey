c643e41085bd17dbd10c0caa6c03473928592584
Jared Halpern <jared@giphy.com>
2017-06-04 08:04:23 -0400

    parser tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -5,3 +5,37 @@ import (
 	"monkey/ast"
 	"monkey/lexer"
 )
+
+func TestLetStatement(t *testing.T) {
+	input := `
+	let x = 5;
+	let y = 10;
+	let foobar = 838383;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")		
+	}
+	if len(program.Statments) != 3 {
+		t.Fatalf("program.Statments does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	test := []struct{
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !TestLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
