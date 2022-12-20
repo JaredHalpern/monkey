795f5b927164c31f3dfe286aeb4f2a7021f53ce0
Jared Halpern <jared@jaredhalpern.com>
2022-12-20 13:53:22 -0500

    wip

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -58,6 +58,9 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
 			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
 		 } else {
 			 fmt.Printf("here - token: %q", l.ch)
 			tok = newToken(token.ILLEGAL, l.ch)
@@ -76,8 +79,20 @@ func (l *Lexer) readIdentifier() string {
 	return l.input[position:l.position]
 }
 
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position: l.position]
+}
+
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
